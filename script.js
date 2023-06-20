4d25998fbda67312e6648f937eb0afb105efe155
mcdott <111994803+mcdott@users.noreply.github.com>
2023-06-20 18:37:34 -0400

    Changes audio input to come from the user's microphone

--- script.js
+++ script.js
@@ -10,49 +10,52 @@ let analyser;
 let frequencyArray;
 
 function startAudio() {
-  const audio = new Audio();
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  audio.src = "birds.mp3";
 
-  // Create an audio analyser
-  analyser = audioContext.createAnalyser();
-  const source = audioContext.createMediaElementSource(audio);
-  source.connect(analyser);
-  analyser.connect(audioContext.destination);
-  frequencyArray = new Uint8Array(analyser.frequencyBinCount);
+  // Get user's microphone
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then((stream) => {
+      // Create an audio analyser
+      analyser = audioContext.createAnalyser();
+      analyser.fftSize = 256;
+      const source = audioContext.createMediaStreamSource(stream);
+      source.connect(analyser);
+      analyser.connect(audioContext.destination);
 
-  audio.play();
-  render();
+      frequencyArray = new Uint8Array(analyser.frequencyBinCount);
+      render();
+    })
+    .catch((err) => {
+      console.log("Error: " + err);
+    });
 }
 
 function render() {
   ctx.clearRect(0, 0, 300, 300);
   ctx.beginPath();
-  ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
-  ctx.strokeStyle = "red";
-  ctx.stroke();
 
   const bars = 200;
   const step = (Math.PI * 2) / bars;
 
   analyser.getByteFrequencyData(frequencyArray);
+  console.log(frequencyArray);
 
   frequencyArray.forEach((f, i) => {
-    const barLength = f * 0.5;
+    const barLength = f;
     const x1 = Math.cos(step * i) * radius + centerX;
     const y1 = Math.sin(step * i) * radius + centerY;
     const x2 = Math.cos(step * i) * (radius + barLength) + centerX;
     const y2 = Math.sin(step * i) * (radius + barLength) + centerY;
 
-    ctx.moveTo(x1, y1);
+    ctx.lineTo(x1, y1);
     ctx.lineTo(x2, y2);
   });
 
+  ctx.strokeStyle = "red";
   ctx.stroke();
   requestAnimationFrame(render);
 }
 
 const playButton = document.getElementById("button-play");
-playButton.addEventListener("click", (e) => {
-  startAudio();
-});
+playButton.addEventListener("click", startAudio);
