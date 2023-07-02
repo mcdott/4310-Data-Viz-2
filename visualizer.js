9c97b6cfb5aaac5dc04596020b63bb08e415a9be
mcdott <111994803+mcdott@users.noreply.github.com>
2023-07-01 20:48:49 -0400

    Sets up microphone and visualizer classes

---
+++ visualizer.js
@@ -0,0 +1,36 @@
+function main() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  class Bar {
+    constructor(x, y, width, height, color) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.color = color;
+    }
+    update(micInput) {
+      this.height = micInput;
+      //   this.x++;
+    }
+    draw(context) {
+      context.fillStyle = this.color;
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+  const bar1 = new Bar(10, 10, 100, 200, "blue");
+  const microphone = new Microphone();
+  console.log(microphone);
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    bar1.draw(ctx);
+    bar1.update();
+    // generate audio samples from the microphone
+    // animate bars based on microphone input data
+    requestAnimationFrame(animate);
+  }
+  animate();
+}
